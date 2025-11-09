0dd26ce46bba47ec3546541a9c9698d81880dfe7
AbdalrhmanSfar <abdalrhmansfar@gmail.com>
2025-11-10 01:02:46 +0300

    fixes/a

--- Assets/Scripts/wavePanelScript.cs
+++ Assets/Scripts/wavePanelScript.cs
@@ -1,18 +1,20 @@
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class wavePanelScript : MonoBehaviour
 {
     private RectMask2D rm;
     [SerializeField] private Wave waveScript;
+    [SerializeField] private float cst;
 
     void Start() {
         rm = GetComponent<RectMask2D>();
     }
 
     void Update() {
-        float x = (waveScript.timeElapsed * 65) / (2*Mathf.PI);
+        float x = (waveScript.timeElapsed * cst) / (2*Mathf.PI);
         // z = right 
-        rm.padding = new Vector4(rm.padding.x, rm.padding.y, 65 - x,rm.padding.w); 
+        rm.padding = new Vector4(rm.padding.x, rm.padding.y, cst - x + 12,rm.padding.w); 
     }
 }
