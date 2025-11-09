cd4d18dc6ddd09736fc89db8cd860149a7996c9a
AbdalrhmanSfar <abdalrhmansfar@gmail.com>
2025-11-10 00:25:19 +0300

    fixed

--- Assets/Scripts/wavePanelScript.cs
+++ Assets/Scripts/wavePanelScript.cs
@@ -5,16 +5,14 @@ public class wavePanelScript : MonoBehaviour
 {
     private RectMask2D rm;
     [SerializeField] private Wave waveScript;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start() {
         rm = GetComponent<RectMask2D>();
-        //waveScript = GameObject.FindGameObjectWithTag("Logic").GetComponent<Wave>();
     }
 
-    // Update is called once per frame
     void Update() {
-        float x = (waveScript.timeElapsed * 247) / (2*Mathf.PI);
+        float x = (waveScript.timeElapsed * 65) / (2*Mathf.PI);
         // z = right 
-        rm.padding = new Vector4(rm.padding.x, rm.padding.y, 247 - x,rm.padding.w); 
+        rm.padding = new Vector4(rm.padding.x, rm.padding.y, 65 - x,rm.padding.w); 
     }
 }
