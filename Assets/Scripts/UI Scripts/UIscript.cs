c436a7897d5d117798795289bca88e8f0edb12c0
Lime <nabelaltabaa@gmail.com>
2025-11-30 03:50:44 +0300

    pause menu

--- Assets/Scripts/UI Scripts/UIscript.cs
+++ Assets/Scripts/UI Scripts/UIscript.cs
@@ -10,6 +10,7 @@ public class UIscript : MonoBehaviour
     private bool paused = false;
     [SerializeField] private GameObject pauseMenu;
     [SerializeField] private GameObject settingsPanel;
+    [SerializeField] private GameObject leaderboardPanel;
     [SerializeField] private Slider masterSlider;
     [SerializeField] private Slider musicSlider;
     [SerializeField] private Slider SFXslider; 
@@ -28,24 +29,44 @@ public class UIscript : MonoBehaviour
     // Update is called once per frame
     void Update() {
         //  JumpDown = input.actions["Jump"].WasPressedThisFrame(),
-        if (input.actions["Pause"].WasPressedThisFrame()) {
+        if (input.actions["Pause"].WasPressedThisFrame()) 
+        {
             Debug.Log("Pause Pressed");
             if (paused) {
                 resumeButton(); 
-            } else {
+            } 
+            else 
+            {
                 pauseMenu.SetActive(true);
                 Time.timeScale = 0; paused = true;
+            }
+        }
+        if (input.actions["Leaderboard"].WasPerformedThisFrame() && paused) 
+        {
+            if (!leaderboardPanel.activeSelf)
+            {
+                leaderboardButton();
+            }
+            else
+            {
+                quitLeaderboard();
             }
         }
     }
 
     public void resumeButton() {
         pauseMenu.SetActive(false);
+        settingsPanel.SetActive(false);
+        leaderboardPanel.SetActive(false);
         Time.timeScale = 1; paused = false;
     }
     public void settingsButton() {
         pauseMenu.SetActive(false);
         settingsPanel.SetActive(true); 
+    }
+    public void leaderboardButton() {
+        pauseMenu.SetActive(false); 
+        leaderboardPanel.SetActive(true);
     }
     public void quitToMain() {
         SceneManager.LoadScene(0); 
@@ -70,4 +91,9 @@ public class UIscript : MonoBehaviour
         pauseMenu.SetActive(true);
         settingsPanel.SetActive(false);
     }
+        public void quitLeaderboard() 
+        {
+        leaderboardPanel.SetActive(false); 
+        pauseMenu.SetActive(true);
+        }
 }
